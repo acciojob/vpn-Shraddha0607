8385047ed3c178f00dbd155b7f0b2ea2993fc7d8
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2024-05-04 21:33:58 +0530

    modified

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -43,16 +43,15 @@ public class AdminServiceImpl implements AdminService {
         serviceProvider.setConnectionList(new ArrayList<>());
         serviceProvider.setUsers(new ArrayList<>());
         serviceProvider.setCountryList(new ArrayList<>());
-//        ServiceProvider serviceProvider1 =
-        serviceProviderRepository1.save(serviceProvider);
+        ServiceProvider serviceProvider1 = serviceProviderRepository1.save(serviceProvider);
 
         // now do connectivity
         Optional<Admin> adminOptional = adminRepository1.findById(adminId);
         Admin admin = adminOptional.get();
-        admin.getServiceProviders().add(serviceProvider);
-        serviceProvider.setAdmin(admin);
+        admin.getServiceProviders().add(serviceProvider1);
+        serviceProvider1.setAdmin(admin);
         adminRepository1.save(admin);
-        serviceProviderRepository1.save(serviceProvider);
+        serviceProviderRepository1.save(serviceProvider1);
 
         return admin;
 
