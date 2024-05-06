700305456aeeea3098264d4fc5c9ec976c602d83
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2024-05-06 19:29:25 +0530

    disconnect api done

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -21,12 +21,40 @@ public class ConnectionServiceImpl implements ConnectionService {
 
     @Override
     public User connect(int userId, String countryName) throws Exception{
-        User user = new User();
+        User user = userRepository2.findById(userId).get();
+        // first if connected already then exception
+        if(user.getConnected() == true) throw new Exception("Already connected");
+
+        // next check country name is same as original country of user
+        else if(user.getOriginalCountry().getCountryName().toString() == countryName) return user;     // no need for connection
+
+        // else part if then do calculation
+//        3. Else, the user should be subscribed under a serviceProvider having option to connect to the given country.
+        //If the connection can not be made (As user does not have a serviceProvider or serviceProvider does not have given country, throw "Unable to connect" exception.
+        //Else, establish the connection where the maskedIp is "updatedCountryCode.serviceProviderId.userId" and return the updated user. If multiple service providers allow you to connect to the country, use the service provider having smallest id.
+
+
+        Country country = user.getOriginalCountry();
+//        if(user.getServiceProviderList()){
+//
+//        }
+//        else{
+//            String maskedIp = country.getCode() + "." + serviceProvider.getId() + "." + user.getId();
+//            user.setMaskedIp(maskedIp);
+//            userRepository2.save(user);
+//        }
+
         return  user;
     }
     @Override
     public User disconnect(int userId) throws Exception {
-        User user = new User();
+        User user = userRepository2.findById(userId).get();
+
+        //If the given user was not connected to a vpn, throw "Already disconnected" exception.
+        if(user.getConnected() ==false) throw new Exception("Already disconnected");
+        //Else, disconnect from vpn, make masked Ip as null, update relevant attributes and return updated user.
+        user.setMaskedIp(null);
+        user.setConnected(false);     // disconnect the vpn
         return user;
     }
     @Override
